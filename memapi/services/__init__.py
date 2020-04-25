2e3cde0142e8a42f02f0d9d2645f76191a3569d9
Mateusz Pianka m.pianka@icloud.com <m.pianka@icloud.com>
2020-04-25 21:43:35 +0200

    services base: check if args are allowed

--- memapi/services/__init__.py
+++ memapi/services/__init__.py
@@ -1,5 +1,7 @@
 import logging
 
+from memapi.utils import get_allowed_args
+
 log = logging.getLogger(__name__)
 
 
@@ -14,14 +16,13 @@ class ServiceProvider:
     def allowed_actions(self) -> list:
         raise NotImplementedError
 
-    def action(self, action_name: str, *args: tuple, **kwargs: dict) -> dict:
+    def action(self, action_name: str, **kwargs: dict) -> dict:
         """
         Action handler. We pass here every action, make checks and then grab result from requested action.
 
         :param action_name: Action name to redirect to. Action function should be named in convention `action_{name}`
-        :param args: Args
         :param kwargs: Kwargs
-        :return: Dict with response
+        :return: Dict with the response.
         """
         log.info(f"Called action {action_name} on {self.SERVICE_NAME} service provider")
 
@@ -32,8 +33,15 @@ class ServiceProvider:
         if not (action_name not in self.allowed_actions) or (not func) or (not callable(func)):
             raise ModuleNotFoundError(f"Action {action_name} does not exist for this service provider")
 
+        # check if params forwarded to the function are allowed
+        allowed_args = get_allowed_args(func)
+        for arg in kwargs:
+            if arg not in allowed_args:
+                raise ValueError(f"Forbidden argument {arg}")
+
+        # return
         # we only accept dicts as a return value, because we serialize everything to JSON afterwards on endpoint side
-        result = func(*args, **kwargs)
+        result = func(**kwargs)
         if not isinstance(result, dict):
             raise TypeError(f"Action result is not in required type. Expected dict, got {str(type(result))}")
 
