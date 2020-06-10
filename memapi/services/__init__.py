8575fbeda1071fc4c346769f800e91decf92c91c
Mateusz Pianka <m.pianka@icloud.com>
2020-06-11 00:28:11 +0200

    service provider refactor (+repostuj.pl)

--- memapi/services/__init__.py
+++ memapi/services/__init__.py
@@ -1,22 +1,234 @@
 import logging
+from typing import List
 
 from memapi.utils import get_allowed_args
 
 log = logging.getLogger(__name__)
 
 
-class ServiceProvider:
-    SERVICE_NAME = None
-    SERVICE_DOMAIN = None
+class StaticPagination:
+    """ pagination class usable when we absolutely know the state of page: items and pages """
 
+    def __init__(
+        self, page: int = None, per_page: int = None, items_count: int = None, **kwargs
+    ) -> None:
+        self.page = page or kwargs.get("page") or 1
+        self.per_page = per_page or kwargs.get("per_page") or 25
+        self.items_count = items_count
+
+    @property
+    def previous_page(self) -> (int, None):
+        """ returns the previous page number if it's not the first one """
+        if self.page == 1:
+            return None
+        return self.page - 1
+
+    @property
+    def next_page(self) -> (int, None):
+        """ returns next page number if it's not the last one (if the results are full)"""
+        if self.items_count < self.per_page:
+            return None
+        return self.page + 1
+
+    @property
+    def as_dict(self) -> dict:
+        return {
+            "page": self.page,
+            "previous_page": self.previous_page,
+            "next_page": self.next_page,
+            "items": self.items_count,
+            "per_page": self.per_page,
+        }
+
+
+class DynamicPagination:
+    """ pagination class to use when the page and/or item count are not known """
+
+    def __init__(
+        self,
+        starting_index: int = None,
+        per_page: int = None,
+        items_count: int = None,
+        **kwargs,
+    ) -> None:
+        self.starting_index = starting_index or kwargs.get("starting_index") or 1
+        self.per_page = per_page or kwargs.get("per_page") or 25
+        self.items_count = items_count
+
+    @property
+    def current_page(self) -> int:
+        return int(self.starting_index / self.per_page)
+
+    @property
+    def previous_index(self) -> (int, None):
+        """ returns the previous page index. returns index or none if this looks like the first page """
+        index = self.starting_index - self.per_page
+        if index <= 0:
+            return None
+        return index
+
+    @property
+    def next_index(self) -> (int, None):
+        """ returns the next starting index or none, if current one looks like the ending one """
+        if self.items_count < self.per_page:
+            return None
+        return self.starting_index + self.per_page
+
+    @property
+    def as_dict(self) -> dict:
+        return {
+            "starting_index": self.starting_index,
+            "previous_index": self.previous_index,
+            "next_index": self.next_index,
+            "items": self.items_count,
+            "per_page": self.per_page,
+            "page": self.current_page,
+        }
+
+
+class ServiceResult:
+    def __init__(
+        self, result: list, pagination: (StaticPagination, DynamicPagination)
+    ) -> None:
+        self.result = result
+        self.pagination = pagination
+
+    @property
+    def as_dict(self) -> dict:
+        return {
+            "result": [i.as_dict for i in self.result],
+            "pagination": self.pagination.as_dict,
+        }
+
+
+class ErrorResult(Exception):
+    """ Class for raising user-visible (public) exceptions. Wrong params, or so """
+
+    def __init__(self, code: str, message: str, instance: object = None) -> None:
+        self.code = code
+        self.message = message
+        self.instance = instance
+
+    def __repr__(self) -> str:
+        return f"<ErrorResult {self.code}>"
+
+    @property
+    def as_dict(self) -> dict:
+        return {
+            "code": self.code,
+            "message": self.message,
+        }
+
+
+class ItemContent:
+    def __init__(
+        self,
+        id: str = None,
+        description: str = None,
+        url: str = None,
+        mimetype: str = None,
+    ):
+        self.id = id
+        self.description = description
+        self.url = url
+        self.mimetype = mimetype
+
+    def __repr__(self):
+        return f"<ItemContent {self.mimetype}: {self.id}>"
+
+    @property
+    def as_dict(self):
+        return {
+            "id": self.id,
+            "description": self.description,
+            "url": self.url,
+            "mimetype": self.mimetype,
+        }
+
+
+class ItemComment:
+    pass
+
+
+class Item:
+    def __init__(
+        self,
+        id: str = None,
+        title: str = None,
+        description: str = None,
+        score: float = None,
+        content: List[ItemContent] = None,
+        url: str = None,
+        comments: List[ItemComment] = None,
+    ):
+        self.id = id
+        self.title = title
+        self.description = description
+        self.score = score
+        self.content = content
+        self.url = url
+        self.comments = comments
+
+    def __repr__(self):
+        return f"<Item: {self.id}>"
+
+    @property
+    def as_dict(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "description": self.description,
+            "score": self.score,
+            "content": [c.as_dict for c in self.content],
+            "url": self.url,
+            "comments": [c.as_dict for c in self.comments],
+        }
+
+
+class ServiceProvider:
     def __init__(self):
-        assert all([self.SERVICE_NAME, self.SERVICE_DOMAIN]), "Service description not set"
+        pass
+
+    @property
+    def service_name(self) -> str:
+        """ Service name """
+        raise NotImplementedError
+
+    @property
+    def service_slug(self) -> str:
+        """ Safe service name """
+        raise NotImplementedError
+
+    @property
+    def service_url(self) -> str:
+        """ URL for service base page """
+        raise NotImplementedError
+
+    @property
+    def pagination_type(self) -> (StaticPagination, DynamicPagination):
+        """ Current pagination type. Returns pagination class. """
+        raise NotImplementedError
+
+    @property
+    def max_score(self) -> (int, float, None):
+        """
+            Set max score for service provider.
+            If platform supports star-based rating, e.g. 6.5/10, set this to it's maximum.
+            Otherwise, if platform doesn't have maximum score - set to None (default)
+        """
+        return None
 
     @property
     def allowed_actions(self) -> list:
-        raise NotImplementedError
+        """ Allowed actions for ServiceProvider """
+        actions = []
+        for name in self.__dict__:
+            if "action_" in name and callable(getattr(self, name)):
+                actions.append(name)
 
-    def action(self, action_name: str, **kwargs: dict) -> dict:
+        return actions
+
+    def action(self, action_name: str, **kwargs: dict) -> ServiceResult:
         """
         Action handler. We pass here every action, make checks and then grab result from requested action.
 
@@ -24,25 +236,37 @@ class ServiceProvider:
         :param kwargs: Kwargs
         :return: Dict with the response.
         """
-        log.info(f"Called action {action_name} on {self.SERVICE_NAME} service provider")
+        log.info(f"Called action {action_name} on {self.service_name} service provider")
 
         action_name = f"action_{action_name}"
 
         # check if function: is in allowed actions, exists and is callable
         func = getattr(self, action_name, None)
-        if not (action_name not in self.allowed_actions) or (not func) or (not callable(func)):
-            raise ModuleNotFoundError(f"Action {action_name} does not exist for this service provider")
+        if (
+            not (action_name not in self.allowed_actions)
+            or (not func)
+            or (not callable(func))
+        ):
+            raise ErrorResult(
+                code="WRONG-ACTION",
+                message=f"Action {action_name} does not exist for this service provider",
+            )
 
         # check if params forwarded to the function are allowed
         allowed_args = get_allowed_args(func)
         for arg in kwargs:
             if arg not in allowed_args:
-                raise ValueError(f"Forbidden argument {arg}")
+                raise ErrorResult(
+                    code="FORBIDDEN-ARG", message=f"Forbidden argument {arg}"
+                )
 
         # return
-        # we only accept dicts as a return value, because we serialize everything to JSON afterwards on endpoint side
+        # we only accept ServiceResults instances as a fuction's return value
         result = func(**kwargs)
-        if not isinstance(result, dict):
-            raise TypeError(f"Action result is not in required type. Expected dict, got {str(type(result))}")
+        if not isinstance(result, ServiceResult):
+            raise TypeError(
+                f"Action result is not in required type. Expected ServiceResult, got {str(type(result))}"
+            )
 
+        # we return ServiceProvider instance in here
         return result
--- memapi/services/repostuj.py
+++ memapi/services/repostuj.py
@@ -3,59 +3,86 @@ import os
 
 from requests import Response
 from requests_html import HTMLSession, HTMLResponse
-from memapi.services import ServiceProvider
+
+from memapi import ServiceProvider
+from memapi.services import (
+    StaticPagination,
+    DynamicPagination,
+    ServiceResult,
+    Item,
+    ItemContent,
+)
 
 log = logging.getLogger(__name__)
 
 
 class Repostuj(ServiceProvider):
-    SERVICE_NAME = "Repostuj"
-    SERVICE_DOMAIN = "repostuj.pl"
-
     def __init__(self):
         super().__init__()
-        self.session = HTMLSession()
+        self.session: HTMLSession = HTMLSession()
 
     @property
-    def allowed_actions(self) -> list:
-        return ["main", "top", "single", "by_tag", "by_author"]
+    def service_name(self) -> str:
+        return "Repostuj"
 
-    def action_main(self, page: int = 1, per_page: int = 25) -> dict:
-        """ grab all elements from the "main" section """
-        return {"results": self._grab_paginated("?top", page, per_page)}
+    @property
+    def service_slug(self) -> str:
+        return "repostuj"
 
-    def action_top(self, page: int = 1, per_page: int = 25) -> dict:
-        """ grab all elements from the "top" section """
-        return {"results": self._grab_paginated("popularne/", page, per_page)}
+    @property
+    def service_url(self) -> str:
+        return "https://repostuj.pl"
 
-    def action_single(self, item_id: str) -> dict:
-        """ Get single element by item_id """
+    @property
+    def pagination_type(self) -> (StaticPagination, DynamicPagination):
+        return DynamicPagination
 
-        url = f"https://repostuj.pl/post/{item_id}"
-        response = self.session.get(url)
+    @property
+    def max_score(self) -> (int, float, None):
+        return None
+
+    def action_main(self, pagination: dict = None) -> ServiceResult:
+        """ Returns mainpage results """
+        pagination = DynamicPagination(**(pagination or dict()))
+
+        return self._grabber("?top", pagination)
+
+    def action_top(self, pagination: dict = None) -> ServiceResult:
+        """ Returns the results from "top" category """
+        pagination = DynamicPagination(**(pagination or dict()))
+
+        return self._grabber("popularne/", pagination)
+
+    # def action_tag(self, tag_name: str, pagination: dict = None) -> ServiceResult:
+    #     """ Returns the results per tag """
+    #     raise NotImplementedError  # TODO: sometimes in the future
+    #
+    # def action_author(self, author_name:str, pagination:dict=None) -> ServiceResult:
+    #     """ Returns the results per author """
+    #     raise NotImplementedError  # TODO: sometimes in the future
+
+    def action_single(self, item_id: str):
+        """ Get single item by Item ID """
+        response = self.session.get(f"{self.service_url}/post/{item_id}")
         current = self._parse_html(response)
 
-        return {"results": [current]}
+        return ServiceResult(
+            result=[current],
+            pagination=DynamicPagination(starting_index=1, per_page=1, items_count=1),
+        )
 
-    def action_by_tag(self, tag_name):
-        pass
+    def _grabber(self, url: str, pagination: DynamicPagination) -> ServiceResult:
+        per_page = pagination.per_page
+        starting_index = pagination.starting_index
 
-    def action_by_author(self, author_name):
-        pass
-
-    def _grab_paginated(self, starting_url: str, page: int, per_page: int) -> list:
-        # change type of params
-        per_page = int(per_page) if isinstance(per_page, str) else per_page
-        page = int(page) if isinstance(page, str) else page
-
-        url = f"https://repostuj.pl/{starting_url}"
+        url = f"https://repostuj.pl/{url}"
         elements = []
-        start_returning_from = (page - 1) * per_page
+        start_returning_from = starting_index
         counter = 0
 
         while True:
             # make request
-            response = self.session.get(url)
+            response: Response = self.session.get(url)
 
             # get next url from current's response's html
             url = self._get_next_url(response.html)
@@ -71,9 +98,11 @@ class Repostuj(ServiceProvider):
 
             # if last element's title is same as current one it means, that we hit the last one - break
             if elements and (
-                elements[-1].get("title") == current.get("title") and elements[-1] == current.get("item_id")
+                elements[-1].title == current.title and elements[-1] == current.item_id
             ):
-                log.debug(f"Found {len(elements)} for required {per_page}, returning results")
+                log.debug(
+                    f"Found {len(elements)} for required {per_page}, returning results"
+                )
                 break
 
             # append current element to list
@@ -86,18 +115,25 @@ class Repostuj(ServiceProvider):
                 break
 
         log.debug(f"Found total of {len(elements)} elements")
-        return elements
+        return ServiceResult(
+            result=elements,
+            pagination=DynamicPagination(
+                starting_index=start_returning_from,
+                per_page=pagination.per_page,
+                items_count=len(elements),
+            ),
+        )
 
-    def _parse_html(self, resp: Response) -> dict:
-        """ parse html page to dict item """
-        return {
-            "id": self._get_id(resp.html),
-            "title": self._get_title(resp.html),
-            "description": None,
-            "score": self._get_score(resp.html),
-            "content": self._get_content(resp.html),
-            "url": self._get_url(resp.html),
-        }
+    def _parse_html(self, response: Response):
+        return Item(
+            id=self._get_id(response.html),
+            title=self._get_title(response.html),
+            description=None,
+            score=self._get_score(response.html),
+            content=self._get_content(response.html),
+            url=self._get_url(response.html),
+            comments=[],  # TODO later
+        )
 
     def _get_id(self, page: HTMLResponse) -> str:
         """ get item id """
@@ -121,7 +157,14 @@ class Repostuj(ServiceProvider):
         if images:
             for i in images:
                 log.debug(f"Found image {i.attrs.get('src')} for {page.url}")
-                items.append(f"https://repostuj.pl{i.attrs.get('src')}")
+                items.append(
+                    ItemContent(
+                        id=self._get_id_from_filename(i.attrs.get("src")),
+                        description=None,
+                        url=f"https://repostuj.pl{i.attrs.get('src')}",
+                        mimetype=self._mimetype_from_ext(i.attrs.get("src")),
+                    )
+                )
 
         # grab all the videos
         if videos:
@@ -129,13 +172,25 @@ class Repostuj(ServiceProvider):
                 source_tag = [
                     e
                     for e in filter(
-                        lambda el: el.attrs.get("type") == "video/mp4", [v] if not isinstance(v, list) else v
+                        lambda el: el.attrs.get("type") == "video/mp4",
+                        [v] if not isinstance(v, list) else v,
                     )
                 ]
                 if len(source_tag):
                     source_tag = source_tag[0]
-                    log.debug(f"Found video {source_tag.attrs.get('src')} for {page.url}")
-                    items.append(f"https://repostuj.pl{source_tag.attrs.get('src')}")
+                    log.debug(
+                        f"Found video {source_tag.attrs.get('src')} for {page.url}"
+                    )
+                    items.append(
+                        ItemContent(
+                            id=self._get_id_from_filename(source_tag.attrs.get("src")),
+                            description=None,
+                            url=f"https://repostuj.pl{source_tag.attrs.get('src')}",
+                            mimetype=self._mimetype_from_ext(
+                                source_tag.attrs.get("src")
+                            ),
+                        )
+                    )
 
         return items
 
@@ -146,3 +201,23 @@ class Repostuj(ServiceProvider):
     def _get_next_url(self, page: HTMLResponse) -> str:
         """ get next page's url """
         return f"https://repostuj.pl{page.find('#post-prev-btn', first=True).attrs.get('href')}"
+
+    def _get_id_from_filename(self, filename: str) -> str:
+        basename = os.path.basename(filename)
+        name, ext = os.path.splitext(basename)
+
+        return name.lower()
+
+    def _mimetype_from_ext(self, filename: str) -> str:
+        extensions = {
+            "mp4": "video/mp4",
+            "jpg": "image/jpeg",
+            "jpeg": "image/jpeg",
+            "png": "image/png",
+            "gif": "image/gif",
+            "webp": "image/webp",
+            "webm": "video/webm",
+        }
+
+        name, ext = os.path.splitext(filename.lower())
+        return extensions.get(ext)
